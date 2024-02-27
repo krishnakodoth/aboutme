125bc67ec3b0b1e551f3816a67cf593dc892e0a0
krisk <krishnaprasad.1.k@nokia.com>
2024-02-27 18:27:36 +0530

    Added google drive cv file

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,11 @@
 import React from "react";
 import profile from "../assets/images/kp.jpg";
 import ProfileTitle from "./ProfileTitle";
-import cv from "../assets/doc/Krishnaprasad.K.pdf"
+// import cv from "../assets/doc/Krishnaprasad.K.pdf"
 import { MdDownloadForOffline } from "react-icons/md";
 
 const Profile = () => {
+  const cv = "https://drive.google.com/file/d/1a8eDu8ZV711rsTVYhjDKAi9ifwFvPV9l/view?usp=sharing";
   return (
     <header>
       <div className="profile-img">
@@ -13,7 +14,7 @@ const Profile = () => {
       <h1>Krishnaprasad K</h1>
       <ProfileTitle />
       
-      <a href={cv} download className="download">
+      <a href={cv} target="_blank" download className="download">
       <MdDownloadForOffline />
         <span>Download CV</span>
         </a>
